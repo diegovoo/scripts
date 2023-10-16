1700d94d3d6c11e0e1272203623af900a29eddfc
diegovoos <dvignerono@gmail.com>
2023-10-16 12:40:35 +0200

    works

--- removeFree.sh
+++ removeFree.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-# rename 's/free-//' free*
-# rename 's/wuolah-free-//' wuolah*
-# rename 's/*-wuolah//' *- # parece que cambia todo por *-wuolah
+rename 's/wuolah-free-//' wuolah-free-*
