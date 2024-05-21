b4c1e312f8a88a12350b40b2a64efec419acb9e1
diegovoos <dvignerono@gmail.com>
2024-05-21 12:16:24 +0200

    UPDATE: vimwiki-to-git has logs

--- vimwiki-to-git.sh
+++ vimwiki-to-git.sh
@@ -3,4 +3,6 @@
 cd ~/OneDrive/vimwiki/
 git add .
 git commit -m "UPDATE: $(date +%Y-%m-%d)"
-git push origin main
+git push origin main 
+
+echo "$(date '+%d-%m-%Y %H:%M:%S') zenbook: (m1tus) CMD(vimwiki-to-git.sh)" >> ~/.log/vimwiki-to-git.log
