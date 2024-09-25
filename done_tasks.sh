394c168743f98c2b0e149407f5a06b93ee142d45
diegovoo <dvignerono@gmail.com>
2024-09-25 16:38:24 +0200

    UPDATE: onedrive --synchronize was deprecated (updated to --sync) ; done_taks to logfile

--- done_tasks.sh
+++ done_tasks.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
 
 input_file="$HOME/OneDrive/vimwiki/index.md"
-output_file="$HOME/OneDrive/vimwiki/done_tasks.md"
+output_file="$HOME/.log/done_tasks.log"
 
 exists_title() {
     grep -n "$1" "$output_file" | cut -d: -f1
 }
 
-mv_title() {
-    local title_line_number
-    if title_line_number=$(exists_title "$1"); then
-        sed -i "${title_line_number}a $2" "$output_file"
-    else
-        echo "$1" >> "$output_file"
-        echo "$2" >> "$output_file"
-    fi
-}
-
 while read -r LINE; do
 
 	if [[ "$LINE" == *"[X]"* ]]; then
-		echo "$LINE" > "$output_file"
+		echo "$LINE" >> "$output_file"
 	fi
 
 done < "$input_file"
