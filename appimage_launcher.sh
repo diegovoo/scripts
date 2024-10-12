1b59c6dbe867c0ff9ea7c03a8a01ea10fd73f521
diegovoo <dvignerono@gmail.com>
2024-10-12 18:20:08 +0200

    UPDATE: appimage launcher + noti when failed

--- appimage_launcher.sh
+++ appimage_launcher.sh
@@ -4,6 +4,11 @@ APPIMAGE_DIR="/home/m1tus/.local/share/appimage/"
 
 APP_NAME="$1"
 
-APPIMAGE="$(ls "$APPIMAGE_DIR"/${APP_NAME}*.AppImage | head -n1)"
+APPIMAGE="$(ls "$APPIMAGE_DIR"/${APP_NAME}*.AppImage 2>/dev/null | head -n1)"
 
-"$APPIMAGE"
+if [ -z "$APPIMAGE" ]; then
+    notify-send "No se encontró AppImage con nombre $APP_NAME"
+else 
+    "$APPIMAGE"
+fi
+
